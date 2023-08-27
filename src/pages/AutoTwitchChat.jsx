4873d54e5b82956a83ea3427a65bec3f4b26d369
JiggypuffFridays <130865466+JiggypuffFridays@users.noreply.github.com>
2023-08-27 00:14:15 -0500

    Ads

--- src/pages/AutoTwitchChat.jsx
+++ src/pages/AutoTwitchChat.jsx
@@ -40,38 +40,43 @@ function AutoTwitchChat() {
             <h1>Auto Twitch Chat Mini</h1>
             <h4 style={{opacity:'60%',marginTop:'-30px'}}>Twitch Chat Message Generator</h4>
             
-            Username
-            <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-            
-            Username Color 
-            <input type="color" value={usernameColor} onChange={e => setUsernameColor(e.target.value)} />
-            
-            Message
-            <textarea value={message} onChange={e => setMessage(e.target.value)} maxLength={500}></textarea>
-            
-            Font
-            <select value={font} onChange={e => setFont(e.target.value)} style={{ fontFamily: font === "Roboto" ? "Roboto" : "inherit" }}>
-                <option value="Default" style={{ fontFamily: "inherit" }}>Default</option>
-                <option value="Roboto" style={{ fontFamily: "Roboto" }}>Roboto</option>
-            </select>
+            <div>
+                Username
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+                
+                Username Color 
+                <input type="color" value={usernameColor} onChange={e => setUsernameColor(e.target.value)} />
+                
+                Message
+                <textarea value={message} onChange={e => setMessage(e.target.value)} maxLength={500}></textarea>
+                
+                Font
+                <select value={font} onChange={e => setFont(e.target.value)} style={{ fontFamily: font === "Roboto" ? "Roboto" : "inherit" }}>
+                    <option value="Default" style={{ fontFamily: "inherit" }}>Default</option>
+                    <option value="Roboto" style={{ fontFamily: "Roboto" }}>Roboto</option>
+                </select>
 
-            <label>
-                <input type="checkbox" checked={dropShadow} onChange={() => setDropShadow(!dropShadow)} />
-                Add Drop Shadow
-            </label>
+                <label>
+                    <input type="checkbox" checked={dropShadow} onChange={() => setDropShadow(!dropShadow)} />
+                    Add Drop Shadow
+                </label>
+            </div>
 
-            <div id="chatbox-preview" style={{ padding: rectanglePadding, width: 'fit-content', fontFamily: font === "Roboto" ? "Roboto" : "inherit" }}>
 
-                <div style={{backgroundColor:'#2a2a2a',padding:'10px',borderRadius:'15px',boxShadow: dropShadow ? '4px 4px 0px rgba(0, 0, 0, .25)' : 'none'}}>
-                    <span style={{ color: usernameColor }}>{username}:</span>&nbsp;
-                    {message.split('\n').map((line, index) => (
-                        <React.Fragment key={index}>
-                            <span style={{color: 'white'}}>{line}</span>
-                            {index !== message.split('\n').length - 1 && <br />}
-                        </React.Fragment>
-                    ))}
+            <div>
+                <div pid="chatbox-preview" style={{ pointerEvents:'none', padding: rectanglePadding, width: 'fit-content', fontFamily: font === "Roboto" ? "Roboto" : "inherit" }}>
+
+                    <div style={{backgroundColor:'#2a2a2a',padding:'10px',borderRadius:'15px',boxShadow: dropShadow ? '4px 4px 0px rgba(0, 0, 0, .25)' : 'none'}}>
+                        <span style={{ color: usernameColor }}>{username}:</span>&nbsp;
+                        {message.split('\n').map((line, index) => (
+                            <React.Fragment key={index}>
+                                <span style={{color: 'white'}}>{line}</span>
+                                {index !== message.split('\n').length - 1 && <br />}
+                            </React.Fragment>
+                        ))}
+                    </div>
+                
                 </div>
-            
             </div>
 
             <button onClick={downloadImage}>Download Image</button>
